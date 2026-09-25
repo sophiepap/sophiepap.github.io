4080fb285dc00697ee267cb636b54ab9708d8c10
unknown <sophie.papoulia98@gmail.com>
2026-09-25 13:36:12 +0300

    added switching between placeholder information and real

--- content/story.ts
+++ content/story.ts
@@ -1,5 +1,4 @@
 import { storyPlaceholder } from "./story.placeholder";
-import { storyPrivate } from "./story.private";
+
 
 export const storyContent = storyPlaceholder;
-//export const storyContent = storyPrivate;
